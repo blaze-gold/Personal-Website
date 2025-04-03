4a8a6c25134b800bc67050d911f1e09d3c4a82f3
blaze <scalablestack007@gmail.com>
2025-04-03 13:22:47 -0400

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { Heading, Flex, Text, Button, Avatar, RevealFx, Arrow, Column } from "@/once-ui/components";
-import { Projects } from "@/components/work/Projects";
+import { Heading, Flex, Text, Button, Avatar, RevealFx, Arrow, Column } from '@/once-ui/components';
+import { Projects } from '@/components/work/Projects';
 
-import { baseURL, routes } from "@/app/resources";
-import { home, about, person, newsletter } from "@/app/resources/content";
-import { Mailchimp } from "@/components";
-import { Posts } from "@/components/blog/Posts";
+import { baseURL, routes } from '@/app/resources';
+import { home, about, person, newsletter } from '@/app/resources/content';
+import { Mailchimp } from '@/components';
+import { Posts } from '@/components/blog/Posts';
 
 export async function generateMetadata() {
   const title = home.title;
@@ -19,7 +19,7 @@ export async function generateMetadata() {
     openGraph: {
       title,
       description,
-      type: "website",
+      type: 'website',
       url: `https://${baseURL}`,
       // images: [
       //   {
@@ -28,12 +28,12 @@ export async function generateMetadata() {
       //   },
       // ],
     },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [ogImage],
-    },
+    // twitter: {
+    //   card: "summary_large_image",
+    //   title,
+    //   description,
+    //   images: [ogImage],
+    // },
   };
 }
 
@@ -45,17 +45,17 @@ export default function Home() {
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "WebPage",
+            '@context': 'https://schema.org',
+            '@type': 'WebPage',
             name: home.title,
             description: home.description,
             url: `https://${baseURL}`,
             image: `${baseURL}/og?title=${encodeURIComponent(home.title)}`,
             publisher: {
-              "@type": "Person",
+              '@type': 'Person',
               name: person.name,
               image: {
-                "@type": "ImageObject",
+                '@type': 'ImageObject',
                 url: `${baseURL}${person.avatar}`,
               },
             },
@@ -75,21 +75,10 @@ export default function Home() {
             </Text>
           </RevealFx>
           <RevealFx translateY="12" delay={0.4} horizontal="start">
-            <Button
-              id="about"
-              data-border="rounded"
-              href="/about"
-              variant="secondary"
-              size="m"
-              arrowIcon
-            >
+            <Button id="about" data-border="rounded" href="/about" variant="secondary" size="m" arrowIcon>
               <Flex gap="8" vertical="center">
                 {about.avatar.display && (
-                  <Avatar
-                    style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
-                    src={person.avatar}
-                    size="m"
-                  />
+                  <Avatar style={{ marginLeft: '-0.75rem', marginRight: '0.25rem' }} src={person.avatar} size="m" />
                 )}
                 {about.title}
               </Flex>
@@ -100,7 +89,7 @@ export default function Home() {
       <RevealFx translateY="16" delay={0.6}>
         <Projects range={[1, 1]} />
       </RevealFx>
-      {routes["/blog"] && (
+      {routes['/blog'] && (
         <Flex fillWidth gap="24" mobileDirection="column">
           <Flex flex={1} paddingLeft="l">
             <Heading as="h2" variant="display-strong-xs" wrap="balance">
